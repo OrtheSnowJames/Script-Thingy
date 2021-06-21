164c3c020cd65bafe9f5206ad08ec8c5bf76f8d4
OrtheSnowJames <71599346+OrtheSnowJames@users.noreply.github.com>
2021-06-21 07:37:23 -0700

    first commit

---
+++ Assets/Scripts/scriptOk.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Bolt;
+using System;
+
+public class ScriptOk : Bolt.GlobalEventListener
+{
+    //Just some varables and metadata or whatever
+
+    [SerializeField]
+    private string _map = "";
+    private static string s_map;
+
+    [SerializeField]
+    private string _roomID  ="Test";
+    private static string s_roomID;
+
+    [SerializeField]
+    private bool _isServer = false;
+
+    public bool isServer {get => _isServer; set => _isServer = value;}
+
+    public static string RoomID()
+    {
+        return s_roomID;
+    }
+
+    public static string Map()
+    {
+        return s_map;
+    }
+    private void Awake()
+    {
+        
+    }
+
+    static string GetArg(params string[] names)
+    {
+        var args = Environment.GetCommandLineArgs();
+        return args[0] ;
+        for (int i = 0; i < args.Length; i ++)
+        {
+            foreach (var name in names)
+            {
+                if (args[i] == name && args.Length > i +1) { return args[i + 1]; }
+            }
+        }
+    }
+   
+}
---
+++ Bolt/Unnessary-Stuff.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Bolt;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Bolt
+{
+    
+    public class GlobalEventListener {
+       
+        private string _bolt = "Photon.Bolt";
+    }
+}
